532782514777eff87e42d49ec0df63da9b2366a0
Niraj Pandit <NirajPandit320>
2026-04-23 13:16:34 +0530

    Fetched the data of the API  properly

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useEffect ,useState} from "react";
 export const Pokemon = () => {
+
+    const[pokemon,setPokemon]=useState([]); 
 
     const API="https://pokeapi.co/api/v2/pokemon?limit=24&offset=24";
 
@@ -8,6 +10,19 @@ export const Pokemon = () => {
             const response =await fetch(API);
             const data=await response.json();
             console.log(data);
+
+            const detailedPokemonData= data.results.map(async(curElem)=>
+            {
+                const res=await fetch(curElem.url);
+                const data= await res.json();
+                return data;    
+            });
+            console.log(detailedPokemonData);
+
+            const detailedData= await Promise.all(detailedPokemonData);
+            console.log(detailedData);
+
+            setPokemon(detailedData);
 
         }catch(error){
             console.error("Error fetching Pokemon:", error);
@@ -19,8 +34,26 @@ export const Pokemon = () => {
     },[])
 
     return (
-        <div>   
-            <h1>Pokemon</h1>
-        </div>
+        <section className="container">
+            <header>
+                <h1>Pokemon Website</h1>
+            </header> 
+
+        
+            <div>
+                <ul className="cards">
+
+                    {pokemon.map((curElem) => (
+
+                    <li key={curElem.id}>
+                        {curElem.name}
+                    </li>
+                        ))}
+
+                </ul>
+            </div>
+        </section>
+        
+        
     );
 }
